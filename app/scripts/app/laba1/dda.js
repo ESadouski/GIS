71dc565dfb1adc1a57bd9cf3cb2b2a6f959645db
Alexander <alexanderrud0603@gmail.com>
2015-10-28 04:26:53 +0300

    Added DDA algorithm

--- app/scripts/app/laba1/dda.js
+++ app/scripts/app/laba1/dda.js
@@ -10,7 +10,7 @@ function doDDA(x1, y1, x2, y2) {
     var incrementX = (x2 - x1) / length;
     var incrementY = (y2 - y1) / length;
 
-    for(var i = 0; i < length; i++) {
+    for (var i = 0; i < length; i++) {
         x1 += incrementX;
         y1 += incrementY;
         squares[Math.floor(x1)][Math.floor(y1)] = false;

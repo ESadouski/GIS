80fd5acdb984e49141728bc908685395e2656d4d
Alexander <alexanderrud0603@gmail.com>
2015-10-29 15:58:46 +0300

    Added debug

--- app/scripts/app/laba1/bresenham.js
+++ app/scripts/app/laba1/bresenham.js
@@ -1,19 +1,30 @@
 function doBresenham(x1, y1, x2, y2) {
-    var dx = Math.abs(x2 - x1), sx = x1 < x2 ? 1 : -1;
-    var dy = Math.abs(y2 - y1), sy = y1 < y2 ? 1 : -1;
-    var err = (dx>dy ? dx : -dy)/2;
+    var usedPixels = [];
+    var index = 0;
+
+    var dx = Math.abs(x2 - x1);
+    var sx = x1 < x2 ? 1 : -1;
+    var dy = Math.abs(y2 - y1);
+    var sy = y1 < y2 ? 1 : -1;
+    var err = (dx > dy ? dx : -dy) / 2;
 
     while (true) {
-        squares[x1][y1] = false;
-        if (x1 === x2 && y1 === y2) {
-            break;
+        usedPixels[index++] = {
+            x: x1,
+            y: y1,
+            e: 1
+        };
+        if (x1 == x2 && y1 == y2) {
+            return usedPixels;
         }
         var e2 = err;
         if (e2 > -dx) {
-            err -= dy; x1 += sx;
+            err -= dy;
+            x1 += sx;
         }
         if (e2 < dy) {
-            err += dx; y1 += sy;
+            err += dx;
+            y1 += sy;
         }
     }
 }

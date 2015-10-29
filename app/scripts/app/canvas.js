6de993f07052c7983ba13eb77304571224b1ad36
Alexander <alexanderrud0603@gmail.com>
2015-10-29 15:58:39 +0300

    Added debug

--- app/scripts/app/canvas.js
+++ app/scripts/app/canvas.js
@@ -1,21 +1,19 @@
-var squares = [];
 var sizeSelect = document.getElementById("cnvSize");
-var lineValue = sizeSelect.options[sizeSelect.selectedIndex].value; //size of square
+var lineValue = sizeSelect.options[sizeSelect.selectedIndex].value; //square size
 var cnv = document.getElementById("cnv");
 var ctx = cnv.getContext("2d");
+var rectCount;
 
-var dots = [4];
-var flag = true;
+var isFirstPixel = true;
+var debugIndex = 0;
+var debugMode = false;
 
-var debugX = 0;
-var debugY = 0;
+const dots = [4];
 
 function drawGrid() {
-    var countOfRect = Math.floor(cnv.height / lineValue);
-    for (var x = 0; x < countOfRect; x++) {
-        squares [x] = [];
-        for (var y = 0; y < countOfRect; y++) {
-            squares [x][y] = true;
+    rectCount = Math.floor(cnv.height / lineValue);
+    for (var x = 0; x < rectCount; x++) {
+        for (var y = 0; y < rectCount; y++) {
             ctx.strokeRect(x * lineValue, y * lineValue, lineValue, lineValue);
         }
     }
@@ -27,72 +25,83 @@ function resizeCnv() {
     drawGrid();
 }
 
-function drawLine(isDebug) {
-    for (var x = debugX; x < squares.length; x++) {
-        for (var y = debugY; y < squares.length; y++) {
-            if (squares[x][y] == false) {
-                ctx.fillRect(x * lineValue, y * lineValue, lineValue, lineValue);
-                if (isDebug) {
-                    debugX = x;
-                    debugY = y;
-                }
-            } else {
-                ctx.clearRect(x * lineValue, y * lineValue, lineValue, lineValue);
-                ctx.strokeRect(x * lineValue, y * lineValue, lineValue, lineValue);
-            }
-        }
+function drawLine(drownPixels) {
+    for (var i = 0; i < drownPixels.length; i++) {
+        plot(drownPixels[i].x, drownPixels[i].y, drownPixels[i].e);
     }
 }
 
+function debugLine(drownPixels) {
+    if (debugIndex == 0) {
+        console.log("Debug mode activated");
+    }
+    if (debugIndex > drownPixels.length) {
+        console.log("Debug was done");
+        return;
+    }
+    const x = drownPixels[debugIndex].x;
+    const y = drownPixels[debugIndex].y;
+    const e = drownPixels[debugIndex].e;
+    plot(x, y, e);
+    console.log("Plot pixel(" + x + ";" + y + ") " + "with intensive " + e);
+    console.log(debugIndex);
+    debugIndex++
+}
+
+function plot(x, y, e) {
+    ctx.fillRect(x * lineValue, y * lineValue, lineValue, lineValue);
+}
+
 function doAlgorithm() {
-    var algorithm = $('input[name="optradio"]:checked').val();
+    const algorithm = $('input[name="optradio"]:checked').val();
+    //window[algorithm].apply(this, dots);
+    //window[algorithm].call();
     switch (algorithm) {
-        case 'DDA' :
-            doDDA(dots[0], dots[1], dots[2], dots[3]);
-            break;
-        case 'Bresenham' :
-            doBresenham(dots[0], dots[1], dots[2], dots[3]);
-            break;
-        case 'VY' :
-            doVY(dots[0], dots[1], dots[2], dots[3]);
-            break;
+        case 'doDDA' :
+            return doDDA(dots[0], dots[1], dots[2], dots[3]);
+        case 'doBresenham' :
+            return doBresenham(dots[0], dots[1], dots[2], dots[3]);
+        case 'doVY' :
+            return doVY(dots[0], dots[1], dots[2], dots[3]);
+
     }
 }
 
 $('#cnv').click(function(event) {
-    //var debug;
-    //$('input[name="debugBtn"]:checked').val() ? debug = true : debug = false;
-    //console.log(debug);
-    var e = event;
-    var drawX = Math.floor(e.offsetX / lineValue);
-    var drawY = Math.floor(e.offsetY / lineValue);
+    debugMode = $('input[name="debugBtn"]:checked').val() ? true : false;
+    const e = event;
+    const drawX = Math.floor(e.offsetX / lineValue);
+    const drawY = Math.floor(e.offsetY / lineValue);
 
-    if (flag) {
+    if (isFirstPixel) {
         dots[0] = drawX;
         dots[1] = drawY;
-        flag = false;
     } else {
         dots[2] = drawX;
         dots[3] = drawY;
-        flag = true;
-        doAlgorithm();
+        debugIndex = 0;
+        if (!debugMode) {
+            drawLine(doAlgorithm());
+        }
     }
-    squares[drawX][drawY] = false;
-    drawLine(false);
+    plot(drawX, drawY, 1);
+    isFirstPixel = !isFirstPixel;
 
     event.preventDefault();
 });
 
-$("nextBtn").click(function() {
-    //doAlgorithm();
-    drawLine(true);
+$("#nextBtn").click(function() {
+    if(debugMode) {
+        debugLine(doAlgorithm());
+    }
+
 });
 
 $("#clearBtn").click(function() {
-    for(var x = 0; x < squares.length; x++) {
-        for(var y = 0; y < squares.length; y++) {
-            squares[x][y] = true;
+    for (var x = 0; x < rectCount; x++) {
+        for (var y = 0; y < rectCount; y++) {
+            ctx.clearRect(x * lineValue, y * lineValue, lineValue, lineValue);
+            ctx.strokeRect(x * lineValue, y * lineValue, lineValue, lineValue);
         }
     }
-    drawLine(false);
 });
